e3b1236546d853bb5b694cc15eb381b537b3a4f4
Vairavandinesh <140646315+Vairavandinesh@users.noreply.github.com>
2025-06-29 19:15:51 +0530

    Update CalculatorTest.java

--- week 2/mockito exercise 2/CalculatorTest.java
+++ week 2/mockito exercise 2/CalculatorTest.java
@@ -7,14 +7,14 @@ public class CalculatorTest {
     public void testVerifyAdd() {
         Calculator mockCalc = mock(Calculator.class);
 
-        // Simulate calls
+        
         mockCalc.add(1, 2);
         mockCalc.add(1, 2);
         mockCalc.clear();
 
-        // ✅ Verifications
-        verify(mockCalc, times(2)).add(1, 2); // called 2 times
-        verify(mockCalc, never()).add(3, 4);  // never called
-        verify(mockCalc).clear();             // called once
+        
+        verify(mockCalc, times(2)).add(1, 2); 
+        verify(mockCalc, never()).add(3, 4);  
+        verify(mockCalc).clear();            
     }
 }
